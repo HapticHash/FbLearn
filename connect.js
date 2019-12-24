ef91c68d158c1f6551ee0afbd3b497cb913bb73d
Harshit Singh <harshitks2203@gmail.com>
2019-12-24 11:27:52 -0500

    init firebase

---
+++ connect.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBwo2kk0DVJM_rrxfEfpCyJL2BkTY5mxC0",
+    authDomain: "fblearn-c0ec2.firebaseapp.com",
+    databaseURL: "https://fblearn-c0ec2.firebaseio.com",
+    projectId: "fblearn-c0ec2",
+    storageBucket: "fblearn-c0ec2.appspot.com",
+    messagingSenderId: "3977348726",
+    appId: "1:3977348726:web:d4e83f8e8839eaf19b8157",
+    measurementId: "G-CQQK0ETTX3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
